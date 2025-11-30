c7ee49126637478c49ac1c5a211914f81e238b39
2456480538@qq.com <2456480538@qq.com>
2025-11-30 16:49:06 +0800

    prepare for gpt

---
+++ gpt/gpt2_generate.py
@@ -0,0 +1,18 @@
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
+
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+model = GPT2LMHeadModel.from_pretrained('gpt2')
+
+input_text = "Once upon a time"
+inputs = tokenizer(input_text, return_tensors='pt')
+
+# 生成
+outputs = model.generate(
+    **inputs,
+    max_new_tokens=50,
+    temperature=0.7,
+    top_p=0.9,
+    do_sample=True
+)
+
+print(tokenizer.decode(outputs[0], skip_special_tokens=True))
